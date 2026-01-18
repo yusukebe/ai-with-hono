6b59c7cfe97bceb5f24fd31f48b3a99c20297fb4
Yusuke Wada <yusuke@kamawada.com>
2026-01-18 18:50:49 +0900

    add stream endpoint

--- projects/chat/src/index.tsx
+++ projects/chat/src/index.tsx
@@ -1,12 +1,30 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import * as z from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.use(renderer)
 
-app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+const schema = z.object({
+  messages: z.array(
+    z.object({
+      role: z.enum(['system', 'user', 'assistant']),
+      content: z.string()
+    })
+  )
+})
+
+app.post('/stream', zValidator('json', schema), async (c) => {
+  const data = c.req.valid('json')
+  const stream = await c.env.AI.run('@cf/meta/llama-3.3-70b-instruct-fp8-fast', {
+    messages: data.messages,
+    stream: true
+  })
+  return c.body(stream, 200, {
+    'Content-Type': 'text/event-stream'
+  })
 })
 
 export default app
--- projects/chat/src/renderer.tsx
+++ projects/chat/src/renderer.tsx
@@ -1,14 +1,17 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
 import { Link, ViteClient } from 'vite-ssr-components/hono'
 
-export const renderer = jsxRenderer(({ children }) => {
-  return (
-    <html>
-      <head>
-        <ViteClient />
-        <Link href="/src/style.css" rel="stylesheet" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
-})
+export const renderer = jsxRenderer(
+  ({ children }) => {
+    return (
+      <html>
+        <head>
+          <ViteClient />
+          <Link href="/src/style.css" rel="stylesheet" />
+        </head>
+        <body>{children}</body>
+      </html>
+    )
+  },
+  { stream: true }
+)
