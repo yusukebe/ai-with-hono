f96b7d277e0a67b817086d6645cd7e7b83257775
Yusuke Wada <yusuke@kamawada.com>
2026-01-26 17:46:24 +0900

    update

--- projects/website/app/routes/_renderer.tsx
+++ projects/website/app/routes/_renderer.tsx
@@ -1,5 +1,5 @@
 import { jsxRenderer } from 'hono/jsx-renderer'
-import { Link, Script } from 'honox/server'
+import { Link } from 'honox/server'
 
 export default jsxRenderer(({ children, frontmatter }) => {
   return (
@@ -8,7 +8,6 @@ export default jsxRenderer(({ children, frontmatter }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
-        <Script src="/app/client.ts" />
         <Link href="/app/style.css" rel="stylesheet" />
         <title>{frontmatter?.title || 'AI with Hono'}</title>
       </head>
